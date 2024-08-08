1502767a234dd424c857900c9e2949b8e538c413
djojujo <159132703+djojujo@users.noreply.github.com>
2024-08-08 08:11:25 -0300

    kj

--- script.js
+++ script.js
@@ -6,15 +6,20 @@
 
  const perguntas = [
     {
-        enunciado:" No ambito social",
+        enunciado:" No ambito social voce prefere",
         alternativas: ["voce prefre....", "ou voce prefere..."]
     },
     {
-        enunciado:" No ambito social",
+        enunciado:" No ambito ambiental voce prefere",
         alternativas: ["Alternativa 1", "Alternativa 2"]
     },
     {
-        enunciado:" No ambito social",
+        enunciado:" No ambito tecnológico voce prefere",
         alternativas: ["Alternativa 1", "Alternativa 2"]
     }
  ]    
+ const media = [120, 130, 140, 110, 30, 50, 4];
+ const calculo = 
+ (media[0]+media[1]+media[2]+media[3]+media[4]+media[5])/6;
+ console.log("A media foi: "+ calculo)
+ 
