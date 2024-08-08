c7c7a8c2ded75615c800e6dac826e758e1ce55c4
djojujo <159132703+djojujo@users.noreply.github.com>
2024-08-08 07:53:38 -0300

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+ const caixaPrincipal = document.querySelector(".caixa-principal");
+ const caixaperguntas = document.querySelector(".caixa-perguntas");
+ const caixaalternativas = document.querySelector(".caixa-alternativas");
+ const caixaresultado = document.querySelector(".caixa-resultado");
+ const textoresultado = document.querySelector(".texto-resultado");
+
+ const perguntas = [
+    {
+        enunciado:" No ambito social",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    },
+    {
+        enunciado:" No ambito social",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    },
+    {
+        enunciado:" No ambito social",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    }
+ ]    
